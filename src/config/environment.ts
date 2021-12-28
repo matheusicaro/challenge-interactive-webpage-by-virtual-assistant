ba9a6af724a88fd9ed0deed6b5bbd09953ecf7c4
matheusicaro <matheusicaro2@hotmail.com>
2021-12-28 19:42:45 -0300

    [ back-end ]: Starting project with initial configs from my template-nodejs-app/graphql-version at https://github.com/matheusicaro/template-nodejs-app/tree/GraphQL/typescript-and-apollo-server.

---
+++ src/config/environment.ts
@@ -0,0 +1,4 @@
+export default {
+  NODE_ENV: process.env.NODE_ENV,
+  PORT: parseInt(process.env.PORT || '')
+};
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+import { ApolloServer } from 'apollo-server';
+import environment from './config/environment';
+import { resolvers, typeDefs } from './graphql/schema';
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.listen(environment.PORT).then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
