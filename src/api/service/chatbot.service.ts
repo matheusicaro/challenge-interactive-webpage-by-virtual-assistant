43fdcd6aa10393976719cc5a5322adaf41cea624
matheusicaro <matheusicaro2@hotmail.com>
2021-12-31 16:55:02 -0300

    [ back-end ]: Adding rules to sending a message in services layer.

--- src/api/service/chatbot.service.ts
+++ src/api/service/chatbot.service.ts
@@ -8,14 +8,14 @@ import ChatbotDatabase from '../integration/chatbot-database.integration';
  */
 class ChabotService {
   /**
-   * Method responsible for sending the message received by the client/user
+   * Class responsible for dealing with business rules related to the exchange of messages between the user and the chatbot.
    *
-   * @param  {string} message: message sent by user/client to the Chatbot
+   * @param  {string} textMessage: message sent by user/client to the Chatbot
    * @param  {string} conversationId: id of session which control the context and the conversation flow.
    *
-   * @returns {Promise<Message>}: promise to return list of AnimalFeeding
+   * @returns {Promise<Message>}
    */
-  public static async sendMessage(message: string, conversationId?: string): Promise<Message> {
+  public static async sendMessage(textMessage: string, conversationId?: string): Promise<Message> {
     const sessionId = conversationId || (await ChatbotIntegration.createSessionId());
     let session = await ChatbotDatabase.getSession(sessionId);
 
@@ -23,14 +23,14 @@ class ChabotService {
       session = new Session(sessionId);
     }
 
-    const answer = await ChatbotIntegration.send(message, sessionId, session.getContext());
+    const answer = await ChatbotIntegration.send(textMessage, sessionId, session.getContext());
 
     session.setContext(answer.getContext());
-    session.addMessage(message, answer.getTextMessages());
+    session.addMessage(textMessage, answer.getTextMessages());
 
     ChatbotDatabase.saveSession(session);
 
-    return new Message(sessionId, message, answer.getTextMessages(), answer.getContext());
+    return new Message(sessionId, textMessage, answer.getTextMessages(), answer.getContext());
   }
 }
 
